ebe4ba12d1ecb4e49eb0321cfded5334aefc8dd7
wasadmin <wasadmin@ip-10-0-1-117>
2023-07-28 18:53:08 +0000

    Event API

--- src/main/java/msd/RestApi.java
+++ src/main/java/msd/RestApi.java
@@ -1,22 +1,44 @@
 package msd;
 
-import java.util.Collection;
+import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
+@Transactional
+@RequestMapping("/api")
 public class RestApi {
 	
-	@Autowired
-	GameSummaryDao gameSummaryDAO;
+	@PersistenceContext
+	private EntityManager entityManager;
 	
-	@GetMapping("/gamesummary")
-	public Collection<GameSummary> getGameSummarys() {
-		return gameSummaryDAO.getAllGameSummaries();
+	@GetMapping("/event")
+	public List<Event> getEvent() {
+		List<Event> allEvent = entityManager.createQuery("from Event", Event.class).getResultList();
+		return allEvent;
 	}
-	
+	@PutMapping("/event/{id}")
+	public void addEvent(@PathVariable int id, @RequestBody Event event) {
+
+		
+		Event newEvent = new Event(event.code, event.title, event.description, event.notes);
+		entityManager.persist(newEvent);
+	}
+	@DeleteMapping("/event/{id}")
+	public void deleteEvent(@PathVariable Long id) {
+		Event eventToDelete = entityManager.find(Event.class, id);
+		entityManager.remove(eventToDelete);
+	}
 }
