b30e17843f917ef52d644cdfcd177f4bc1520fd3
Miki <cheng_miki@bah.com>
2023-08-01 19:04:04 +0000

    add customer service

--- src/main/java/com/msd/App.java
+++ src/main/java/com/msd/App.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com", "com.msd", "com.msd.rest", "com.msd.service", "com.msd.service.registration", "com.msd.model"})
+@ComponentScan(basePackages = {"com", "com.msd", "com.msd.rest", "com.msd.service", "com.msd.service.customer", "com.msd.service.registration", "com.msd.model"})
 public class App {
 	
 	public static void main(String[] args) {
